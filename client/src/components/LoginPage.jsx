8cc73b834aaccbba59fb86100c63943b714c373e
Sathvik Reddy <sbr87644@gmail.com>
2024-10-08 21:16:19 -0500

    Implemented basic front end design for Login Page

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -7,8 +8,22 @@ function LoginPage() {
     navigate('/home');
   }
     return (
-      <div>
-      <h1>Welcome to the Login Page</h1>
+      <div className = "login-container">
+        <div className = "login-box">
+          <h2> Login to your JESSA Account</h2>
+          <div className = "login-field">
+            <label>Username :</label>
+            <input type = "text" />
+          </div>
+          <div className = "login-field">
+            <label>Password :</label>
+            <input type = "password" />
+          </div>
+          <div className = "login-actions">
+            <button className = "login-button">LOG IN</button>
+            <button className = "new-user-button">NEW USER</button>
+          </div>
+        </div>
       <button onClick={goToAboutPage}>"Go To Home Page"</button>
     </div>
     );
